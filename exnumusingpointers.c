5cf90acb077e597aa6fb16bf4aa541c553afde46
pushpitshukla28 <pushpitshukla28@gmail.com>
2024-09-10 00:46:22 +0530

    Without Pointer

--- exnumusingpointers.c
+++ exnumusingpointers.c
@@ -1,4 +1,4 @@
-/*#include <stdio.h>
+#include <stdio.h>
 int exchange(int x, int y){
     int temp;
     temp = x;
@@ -15,22 +15,7 @@ int exchange(int x, int y){
     int y = exchange(a,b);
     printf("The exchanged numbers are:%d %d",a ,b);
 
- }*/
+ }
 
-#include <stdio.h>
 
-void exchange(int *x, int *y) {
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-}
 
-int main() {
-    int a, b;
-    printf("Enter two numbers to be exchanged: \n");
-    scanf("%d %d", &a, &b);
-    exchange(&a, &b);
-    printf("The exchanged numbers are: %d %d\n", a, b);
-    return 0;
-}
