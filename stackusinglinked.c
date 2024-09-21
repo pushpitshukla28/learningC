8f4bbec8d93e499bfdb9eb2993cd99ce12db9f43
pushpitshukla28 <pushpitshukla28@gmail.com>
2024-09-21 17:30:58 +0530

    Stack Using Linked list but pointer to pointer

---
+++ stackusinglinked.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+}node;
+
+void traversal(node *ptr){
+    while(ptr != NULL){
+        printf("Element is %d\n" , ptr->data);
+        ptr= ptr->next;
+    }
+
+}
+
+int isEmpty(node *top){
+    if(top == NULL){
+        return 1;
+    }
+    else
+    return 0;
+}
+
+int isFull(node *top){
+    node *p = (node *)malloc(sizeof(node));
+    if(p== NULL){
+        return 1;
+    }
+    else
+    return 0;
+}
+
+
+
+//Push is insterting node @ index 0
+node *push(node *top ,int x){
+    if(isFull(top)){
+        printf("Stack Overflow\n");
+    }
+    else{
+        node *n = (node *)malloc(sizeof(node));
+        n->data = x;
+        n->next = top;
+        top = n;
+        return top;
+    }
+
+}
+//Pop is printing value at index 0 and removing it using free()
+int pop(node **top){
+    if(isEmpty(*top)){
+        printf("Stack Underflow\n");
+    }
+    else{
+        node *n = *top;
+        *top = (*top)->next;
+        int x = n->data;
+        free(n);
+        return x;
+    }
+}
+int main(){
+    node *top = NULL;
+    top = push(top , 7);
+    top = push(top , 8);
+    top = push(top , 12);
+    top = push(top , 69);
+
+    traversal(top);
+    int element = pop(&top);
+    printf("The popped element is %d" , element);
+
+    printf("\nStack after pop\n");
+    traversal(top);
+
+
+}
+
