720107915d9a780bffdca9f987728a660844b714
pushpitshukla28 <pushpitshukla28@gmail.com>
2024-10-21 12:21:52 +0530

    Searching in BST using Iterative

---
+++ BSTsearchIter.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data){
+    struct node *n;
+    n = (struct node *) malloc (sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void preOrder(struct node *root){
+    if(root != NULL){
+        printf("%d " , root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct node*root){
+    if(root != NULL){
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d " , root->data);
+    }
+}
+
+void inOrder (struct node *root){
+    if(root != NULL){
+        inOrder(root->left);
+        printf("%d " , root->data);
+        inOrder(root->right);
+    }
+}
+
+int isBST(struct node*root){
+    static struct node *prev = NULL;
+    if(root != NULL){
+        if( !isBST(root->left)){
+            return 0;
+        }
+        if(prev != NULL && root->data <= prev->data){
+            return 0;
+        }
+        prev = root;
+        return isBST(root->right);
+    }
+    else{
+        return 1;
+    }
+}
+
+struct node *searchIter(struct node*root , int key){
+    while(root !=NULL){
+        if(key == root->data){
+            return root;
+        }
+        else if(key < root->data){
+            root = root->left;
+        }
+        else{
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+
+
+int main(){
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
+
+    /*
+    Tree
+         5
+         /\                   This is a binary search tree.
+       3   6
+      / \
+     1   4
+    */
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    // printf("The Pre Order traversal is\n");
+
+    // preOrder(p);
+    // printf("\nThe Post Order traversal is\n");
+    // postOrder(p);
+    printf("\nThe In Order traversal is\n");
+    inOrder(p);
+
+    printf("%d" , isBST(p));
+
+    struct node *n = searchIter(p,1);
+    if(n!= NULL){
+        printf("\nFound: %d\n" , n->data);
+    }
+    else{
+        printf("\nElement not found\n");
+    }
+
+
+}
