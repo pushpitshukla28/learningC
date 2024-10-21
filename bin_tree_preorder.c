24a17ee2d5e81be0647ffd1b4cc5a7817320057b
pushpitshukla28 <pushpitshukla28@gmail.com>
2024-10-21 12:19:46 +0530

    Binary Tree Pre, Post and InOrder

---
+++ bin_tree_preorder.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <malloc.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data){
+    struct node *n;
+    n = (struct node *) malloc (sizeof(struct node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+void preOrder(struct node *root){
+    if(root != NULL){
+        printf("%d " , root->data);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+void postOrder(struct node*root){
+    if(root != NULL){
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("%d " , root->data);
+    }
+}
+
+void inOrder (struct node *root){
+    if(root != NULL){
+        inOrder(root->left);
+        printf("%d " , root->data);
+        inOrder(root->right);
+    }
+}
+
+
+int main(){
+    struct node *p = createNode(4);
+    struct node *p1 = createNode(1);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(5);
+    struct node *p4 = createNode(2);
+
+    /*
+    Tree
+         4
+         /\
+       1   6
+      / \
+     5   2
+    */
+
+    p->left = p1;
+    p->right = p2;
+    p1->left = p3;
+    p1->right = p4;
+
+    printf("The Pre Order traversal is\n");
+
+    preOrder(p);
+    printf("\nThe Post Order traversal is\n");
+    postOrder(p);
+    printf("\nThe In Order traversal is\n");
+    inOrder(p);
+}
